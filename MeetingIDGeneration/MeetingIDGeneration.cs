92661d3ee8cca26d18950f18e66d2b2f7a630b2f
sandeep r <rsreddykm@gmail.com>
2020-09-03 23:50:23 -0500

    added interface

--- MeetingIDGeneration/MeetingIDGeneration.cs
+++ MeetingIDGeneration/MeetingIDGeneration.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
 
 namespace MeetingIDGeneration
 {
-    public class Class1
+    public class MeetingIDGeneration : IPlugin
     {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+           //Business Logic goes here
+        }
     }
 }
